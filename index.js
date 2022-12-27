51b87950008d3cfa7d74cd2e340d94384af673fe
yujin <yjcho@cavyos.com>
2022-12-27 18:30:55 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,190 @@
+// Require the necessary discord.js classes
+const {
+  Client,
+  Events,
+  GatewayIntentBits,
+  strikethrough,
+  bold,
+  SlashCommandBuilder,
+  REST,
+  Routes,
+  Collection,
+  EmbedBuilder,
+} = require("discord.js");
+const dotenv = require("dotenv");
+const { default: axios } = require("axios");
+
+const { JSDOM } = require("jsdom");
+const iconv = require("iconv-lite");
+
+dotenv.config();
+
+const token = process.env.DISCORD_TOKEN;
+const clientId = process.env.DISCORD_CLIENT_ID;
+const PPOMPPU_BASE_URL = "https://www.ppomppu.co.kr/zboard/";
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, async (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(token);
+
+const makePpomppuTopMessage = async () => {
+  const response = await axios.get(process.env.PPOMPPU_CRAWLING_URL, {
+    responseType: "arraybuffer",
+    responseEncoding: "binary",
+  });
+  const buffer = new Buffer.from(response.data, "binary");
+
+  const document = new JSDOM(iconv.decode(buffer, "euc-kr").toString()).window
+    .document;
+
+  const itemsParsed = [];
+
+  for (const item of [
+    ...document.getElementsByClassName("list1"),
+    ...document.getElementsByClassName("list0"),
+  ]) {
+    try {
+      const idText = String(
+        item.getElementsByClassName("eng list_vspace")[0].textContent
+      ).trim();
+      const id = Number.parseInt(idText);
+      if (Number.isNaN(id)) continue;
+
+      const dateElement = item.getElementsByClassName("eng list_vspace")[1];
+      if (dateElement === undefined) continue;
+
+      const dateStrinbg =
+        "20" +
+        dateElement
+          .getAttribute("title")
+          .replace(" ", "T")
+          .replace(".", "-")
+          .replace(".", "-");
+
+      const date = new Date(dateStrinbg);
+
+      if (new Date().getTime() - date.getTime() > 1000 * 60 * 60 * 12) {
+        continue;
+      }
+    } catch (error) {
+      console.error(error);
+      continue;
+    }
+
+    const titleElement = item?.getElementsByClassName?.("list_title")?.[0];
+    const title = titleElement?.textContent;
+    if (title === undefined) continue;
+    const url = titleElement.parentElement.href;
+    if (title === undefined) continue;
+    itemsParsed.push({ title, url: PPOMPPU_BASE_URL + url });
+  }
+
+  const today = new Date();
+
+  const exampleEmbed = new EmbedBuilder()
+    .setColor(0x0099ff)
+    .setTitle("실시간 뽐뿌 할인 정보")
+    .setDescription(
+      `${today.toLocaleString("ko")} 최근 12시간 내 인기있는 할인 정보입니다.`
+    )
+    .setTimestamp();
+
+  for (const item of itemsParsed) {
+    exampleEmbed.addFields({
+      name: item.title,
+      value: item.url,
+    });
+  }
+
+  return exampleEmbed;
+};
+
+const makeSteamSaleMessage = async () => {
+  // console.log(channel);
+
+  const response = await Promise.all([
+    await axios.get(process.env.STEAM_API_URL + "?page=1&size=5"),
+    await axios.get(process.env.STEAM_API_URL + "?page=2&size=5"),
+  ]);
+
+  const today = new Date();
+
+  const games = [...response[0].data.list, ...response[1].data.list];
+
+  const exampleEmbed = new EmbedBuilder()
+    .setColor(0x0099ff)
+    .setTitle("오늘의 인기 스팀 할인 정보")
+    .setDescription(
+      `${today.toLocaleDateString("ko")} 인기 할인 게임 정보입니다.`
+    )
+    .setTimestamp();
+
+  for (const game of games) {
+    exampleEmbed.addFields({
+      name: game.title_nm,
+      value:
+        bold(`[${Number(game.discount_rt) * 100}%]`) +
+        ` ${strikethrough(
+          game.full_price_va.toLocaleString("ko")
+        )}원 :point_right: ${game.sale_price_va.toLocaleString("ko")}원\n${
+          game.store_lk.split("?")[0]
+        }`,
+    });
+  }
+
+  return exampleEmbed;
+};
+
+const commands = [
+  {
+    data: new SlashCommandBuilder()
+      .setName("뽐뿌")
+      .setDescription("현재 뽐뿌에서 인기있는 할인 정보를 확인합니다."),
+    async execute(interaction) {
+      const embed = await makePpomppuTopMessage();
+      await interaction.reply({ embeds: [embed] });
+    },
+  },
+  {
+    data: new SlashCommandBuilder()
+      .setName("스팀")
+      .setDescription("오늘의 인기 스팀 할인 정보를 확인합니다."),
+    async execute(interaction) {
+      const embed = await makeSteamSaleMessage();
+      await interaction.reply({ embeds: [embed] });
+    },
+  },
+];
+
+client.commands = new Collection();
+
+commands.forEach((command) => client.commands.set(command.data.name, command));
+
+const rest = new REST({ version: "10" }).setToken(token);
+
+rest.put(Routes.applicationCommands(clientId), {
+  body: commands.map((command) => command.data.toJSON()),
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
